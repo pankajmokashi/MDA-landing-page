d700c6c749c824fdecda49584ad49865993cab6e
pankajmokashi <pankajmokashi12@gmail.com>
2024-11-05 22:37:29 +0530

    Add mobile nav dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import Button from "./Button";
+import { useEffect, useRef, useState } from "react";
 
 const navLinks = [
   {
@@ -21,11 +22,36 @@ const navLinks = [
 ];
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 640 && setIsOpen(false)
+    );
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Add event listener for clicks outside
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <header className="w-full px-8 py-1">
-      <nav className="mx-auto w-full max-w-screen-xl flex items-center justify-between text-sm">
+      <nav className="relative mx-auto w-full max-w-screen-xl flex items-center justify-between text-sm">
         <div>
           <Link to="/">
             <img className="w-[160px] xm:w-full" src="/logo1.svg" alt="logo" />
@@ -49,7 +75,10 @@ function Header() {
         <div className="hidden md:block">
           <Button name="Login" background="black" />
         </div>
-        <div className="block md:hidden cursor-pointer">
+        <div
+          className="block md:hidden cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <svg
             className="w-6 h-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -59,6 +88,34 @@ function Header() {
             <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
           </svg>
         </div>
+        {isOpen && (
+          <div
+            ref={dropdownRef}
+            className="absolute top-16 w-full bg-white text-black xm:mt-4 p-4 rounded-lg z-10 shadow-2xl"
+          >
+            <ul className="flex flex-col pb-2">
+              {navLinks.map((item) => (
+                <li key={item.name} className="w-full p-2 flex justify-center">
+                  <Link
+                    to={item.link}
+                    className={`mx-auto ${
+                      location.pathname === item.link ? "font-bold" : ""
+                    }`}
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="">
+              <button
+                className={`relative group w-full transition duration-300 p-2 bg-black text-white rounded-xl`}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
